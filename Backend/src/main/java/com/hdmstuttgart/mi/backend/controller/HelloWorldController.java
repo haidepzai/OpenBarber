3871fadc4817017cb172d68fa4b082634b73c363
Hai Vu <zerato@hotmail.de>
2022-11-02 20:02:55 +0100

    Added first controller

---
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.hdmstuttgart.mi.backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String hello(@RequestParam(required = false, defaultValue = "") String text,
+                        @RequestParam(required = false, defaultValue = "") String text2) {
+        if (text.isEmpty() && text2.isEmpty())  {
+            return "Application is up and running. ";
+        }
+
+        return "You entered: "+text + " "+ text2;
+    }
+
+
+}
