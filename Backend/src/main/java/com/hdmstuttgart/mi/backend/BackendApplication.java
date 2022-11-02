faebd4afdbb4d7392a82dfc94a12ced2ad5feff9
Hai Vu <zerato@hotmail.de>
2022-11-02 19:56:48 +0100

    Added structure and first endpoint to Backend

--- Backend/src/main/java/com/hdmstuttgart/mi/backend/BackendApplication.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/BackendApplication.java
@@ -1,10 +1,26 @@
 package com.hdmstuttgart.mi.backend;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import org.slf4j.Logger;
+
+@RestController
+@EnableAutoConfiguration
+@EnableJpaAuditing
 public class BackendApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
