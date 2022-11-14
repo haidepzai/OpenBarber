f662d30dd046ebf3ccfb10b4355c2075460800e1
David Redinger <d.redinger@outlook.de>
2022-11-14 16:49:13 +0100

    Reviewed inital DetailPage Changes Added React-Dom-Router Added inital LandingPage Component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './css/App.css';
 import DetailPage from './pages/DetailPage/DetailPage';
+import LandingPage from './pages/LandingPage/LandingPage';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <DetailPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="shops/*" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <p>Landing Page Placeholder</p>
+    </div>
+  )
+}
+
+export default LandingPage
