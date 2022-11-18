bfa3dbaefeb2faa4cacf57b34773a8d20569cf8a
Vidovic Kristian <kv021@hdm-stuttgart.de>
2022-11-18 13:02:51 +0100

    added lombok annotations to enterprise class

--- Backend/src/main/java/com/hdmstuttgart/mi/backend/model/Enterprise.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/model/Enterprise.java
@@ -1,16 +1,22 @@
 package com.hdmstuttgart.mi.backend.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Enterprise {
+
     @Id
     @GeneratedValue
     private long id;
 
+    @NonNull
     private String name;
 }
