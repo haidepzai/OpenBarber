bb43352dc71229593fbbd411684d0adb5730659f
nlfmt <nlfmt@gmx.de>
2022-11-21 02:20:02 +0100

    break up detail view into smaller components

--- Frontend/src/pages/DetailPage/components/DetailPageBG.js
+++ Frontend/src/pages/DetailPage/components/DetailPageBG.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-import { Box } from '@mui/material';
+import {
+  Box,
+} from '@mui/material'
+
 
 const DetailPageBG = ({ img }) => {
-  return <Box className="detailPageBG" sx={{ backgroundImage: `url(${img})` }} />;
-};
+  return (
+    <Box sx={{
+        backgroundSize: "cover",
+        backgroundImage: `url(${img})`,
+        backgroundPosition: "center center",
+        
+        width: "100%",
+        height: "40vh",
+        position: "absolute",
+        top: 0, left: 0,
+      }}
+    />
+  )
+}
 
-export default DetailPageBG;
+export default DetailPageBG
