c9781864f626e1cbc3459210e9414805d101ab80
ds189 <Enton!Flegm0n>
2022-11-21 02:58:45 +0100

    landing-page first results

--- Frontend/src/pages/LandingPage/LandingPage.js
+++ Frontend/src/pages/LandingPage/LandingPage.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import PlaceHolderComponent from './components/PlaceHolderComponent';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Search from "./components/Search";
+import {Box, Typography} from "@mui/material";
+import MySwiper from "./components/MySwiper";
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    /* colors from https://m2.material.io/inline-tools/color/ */
+    primary: {
+      main: '#6D5344',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#445e6d',
+    },
+    analogous: {
+      main: '#6d4449'
+    },
+    white: {
+      main: '#fff'
+    }
+  }
+})
 
 const LandingPage = () => {
   return (
-    <div>
-      <p>Landing Page Placeholder</p>
-      <PlaceHolderComponent />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Search />
+      <Box sx={{ maxWidth: "1500px", margin: "0 auto", padding: "0px 50px" }}>
+        <Typography variant="h5" sx={{ paddingBottom: "10px", borderBottom: "1px solid rgba(0,0, 0, 0.3)", m: "20px 0" }}>
+          Which barbers would you like to see?
+        </Typography>
+        <MySwiper />
+        <Typography variant="h5" sx={{ paddingBottom: "10px", borderBottom: "1px solid rgba(0,0, 0, 0.3)", m: "20px 0" }}>
+          Top Barbers near your location
+        </Typography>
+      </Box>
+    </ThemeProvider>
   );
 };
 
---
+++ Frontend/src/pages/LandingPage/components/MySwiper.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../../css/components/Swiper.css"
+// import required modules
+import { Pagination } from "swiper";
+
+import beardImage from "../assets/beard.jpg"
+import ratingsImage from "../assets/rating.jpg"
+import mostImage from "../assets/most.jpg"
+import priceImage from "../assets/price.jpg"
+import locationImage from "../assets/location.jpg"
+import tipsImage from "../assets/tips.jpg"
+import {Typography, Box} from "@mui/material";
+
+function MySwiper(props) {
+
+    const criteria = [
+        {name: "Best Ratings", src: ratingsImage},
+        {name: "Most Ratings", src: mostImage},
+        {name:"Cheapest Prices", src: priceImage},
+        {name: "Near you", src: locationImage},
+        {name: "Our personal tips", src: tipsImage},
+        {name: "With Beard Trimming", src: beardImage}]
+
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={0}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+        >
+            {criteria.map(({ name, src}) => (
+                <SwiperSlide style={{}}>
+                    <Box sx={{
+                        padding: "0",
+                        margin: "15px",
+                        height: "400px",
+                        width: "400px",
+                        background: `url(${src})`,
+                        backgroundSize: "cover",
+                        backdropFilter: "blur(5px)",
+                        position: "relative",
+                        borderRadius: "10px",
+                        boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}>
+                        <Typography variant="h3" sx={{ textAlign: "center", position: "absolute", top: "50%",left: "50%", transform: "translate(-50%, -50%)", margin: "0", color: "white.main" }}>
+                            {name}
+                        </Typography>
+                    </Box>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+}
+
+export default MySwiper;
