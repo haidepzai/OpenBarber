1919a5791bd1a9e6c8a9d3f01814a2fd13afade0
ds189 <Enton!Flegm0n>
2022-12-11 00:13:32 +0100

    Footer done

--- Frontend/src/pages/components/Footer.js
+++ Frontend/src/pages/components/Footer.js
@@ -36,38 +36,41 @@ const footerTheme = createTheme({
 function Footer(props) {
     return (
         <ThemeProvider theme={footerTheme}>
-        <Stack direction="row" justifyContent="space-between" sx={{ backgroundColor: "#6D5344", p: "36px 48px" }}>
-            <Stack direction="column" gap={2}>
-                <img src={image} alt="Openbarber-Logo" width={"30px"} style={{ filter: "brightness(0) invert(1)" }}/>
-                <Stack direction="row" gap={3}>
-                    <Link href="#" underline="hover">Impressum</Link>
-                    <Link href="#" underline="hover">Datenschutz</Link>
-                    <Link href="#" underline="hover">Nutzungsbedingungen</Link>
-                    <Link href="#" underline="hover">Kontakt</Link>
-                    <Link href="#" underline="hover">FAQs</Link>
+            <Stack direction="row" justifyContent="space-between" sx={{ backgroundColor: "#6D5344", p: "36px 48px", mt: "96px" }}>
+                <Stack direction="column" gap={2}>
+                    <Stack direction="row" gap={2} alignItems="center">
+                        <img src={image} alt="OpenBarber-Logo" width={"25px"} style={{ filter: "brightness(0) invert(1)" }} />
+                        <Typography variant="h5" sx={{color:"white.main"}} >OpenBarber</Typography>
+                    </Stack>
+                    <Stack direction="row" gap={3}>
+                        <Link href="#" underline="hover">Impressum</Link>
+                        <Link href="#" underline="hover">Datenschutz</Link>
+                        <Link href="#" underline="hover">Nutzungsbedingungen</Link>
+                        <Link href="#" underline="hover">Kontakt</Link>
+                        <Link href="#" underline="hover">FAQs</Link>
+                    </Stack>
+                    <Typography variant="p" sx={{ color: "#BCA7AF" }}>
+                        Copyright © 2022 OpenBarber, Nobelstraße 10, 70569 Stuttgart, Alle Rechte vorbehalten.
+                    </Typography>
                 </Stack>
-                <Typography variant="p" sx={{ color: "#BCA7AF" }}>
-                    Copyright © 2022 OpenBarber, Nobelstraße 10, 70569 Stuttgart, Alle Rechte vorbehalten.
-                </Typography>
-            </Stack>
-            <Stack direction="column" gap={3}>
-                <Button variant="contained" color="white" sx={{ color: "#6D5344" }}>Für Unternehmen</Button>
-                <Stack direction="row" gap={1}>
-                    <IconButton aria-label="delete" size="large">
-                        <InstagramIcon fontSize="inherit"/>
-                    </IconButton>
-                    <IconButton aria-label="delete">
-                        <TwitterIcon />
-                    </IconButton>
-                    <IconButton aria-label="delete">
-                        <FacebookIcon />
-                    </IconButton>
-                    <IconButton aria-label="delete">
-                        <LinkedInIcon />
-                    </IconButton>
+                <Stack direction="column" gap={3}>
+                    <Button variant="contained" color="white" sx={{ color: "#6D5344" }}>Für Unternehmen</Button>
+                    <Stack direction="row" gap={1}>
+                        <IconButton aria-label="delete" size="large">
+                            <InstagramIcon fontSize="inherit"/>
+                        </IconButton>
+                        <IconButton aria-label="delete">
+                            <TwitterIcon />
+                        </IconButton>
+                        <IconButton aria-label="delete">
+                            <FacebookIcon />
+                        </IconButton>
+                        <IconButton aria-label="delete">
+                            <LinkedInIcon />
+                        </IconButton>
+                    </Stack>
                 </Stack>
             </Stack>
-        </Stack>
         </ThemeProvider>
     );
 }
