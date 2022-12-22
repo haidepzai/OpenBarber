8c6595548926945c7c4cc15070d540e6de8ea854
Hai Vu <zerato@hotmail.de>
2022-12-22 17:43:10 +0100

    Fixed Header

--- Frontend/src/pages/components/Header.js
+++ Frontend/src/pages/components/Header.js
@@ -8,11 +8,11 @@ import { blueGrey } from '@mui/material/colors';
 const Header = () => {
   return (
     <>
-      <Grid container columns={16} sx={{ borderBottom: 1, borderColor: 'grey.300' }}>
+      <Grid container columns={16} sx={{ borderBottom: 1, borderColor: 'grey.300', paddingLeft: "10%", paddingRight: "10%" }}>
         <Grid xs={8}>
           <Stack direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={1} m ml="15px">
             <a href="/">
-              <img src={image} width="50px"></img>
+              <img src={image} width="50px" alt="logo"></img>
             </a>
             <a href="/">
               <Typography mt="10px !important" variant="h5" fontFamily="Roboto" fontWeight="500">
