2c2bd1736bbf04cbc93557f6949c60b58be9990f
Hai <sv048@hdm-stuttgart.de>
2022-12-22 12:47:21 +0100

    Made Card Component clickable

--- Frontend/src/pages/components/CardComponent/MediaCard.js
+++ Frontend/src/pages/components/CardComponent/MediaCard.js
@@ -5,46 +5,47 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Box, Rating } from '@mui/material';
+import { Box, CardActionArea, Rating } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom'
 
-export default function MediaCard({image, title, rating, description}) {
+export default function MediaCard({ image, title, rating, description, link }) {
   return (
     <Card sx={{ maxWidth: 250, borderRadius: 3 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={image}
-        alt="Barber"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <Rating readOnly precision={0.5} value={rating} sx={{ color: 'primary.main' }} size="small" />
-          <Typography variant="span" style={{ fontWeight: 600 }} color="grey.400">
-            5 Reviews
+      <CardActionArea component={RouterLink} to={link}>
+        <CardMedia component="img" height="140" image={image} alt="Barber" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
           </Typography>
-        </Box>
 
-        <Typography
-          sx={{
-            display: '-webkit-box',
-            overflow: 'hidden',
-            WebkitBoxOrient: 'vertical',
-            WebkitLineClamp: 2,
-          }} 
-          mt={2} 
-          variant="body2" 
-          color="text.secondary">
-           {description}
-        </Typography>
-      </CardContent>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <Rating readOnly precision={0.5} value={rating} sx={{ color: 'primary.main' }} size="small" />
+            <Typography variant="span" style={{ fontWeight: 600 }} color="grey.400">
+              5 Reviews
+            </Typography>
+          </Box>
 
-      <CardActions>
-        <Button variant="contained" color="primary">Book Now</Button>
-      </CardActions>
+          <Typography
+            sx={{
+              display: '-webkit-box',
+              overflow: 'hidden',
+              WebkitBoxOrient: 'vertical',
+              WebkitLineClamp: 2,
+            }}
+            mt={2}
+            variant="body2"
+            color="text.secondary"
+          >
+            {description}
+          </Typography>
+        </CardContent>
+
+        <CardActions>
+          <Button variant="contained" color="primary">
+            Book Now
+          </Button>
+        </CardActions>
+      </CardActionArea>
     </Card>
   );
 }
