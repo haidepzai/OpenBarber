962d6dfedafb894066f60ed5bb68791d9f92912b
Hai Vu <zerato@hotmail.de>
2022-12-23 18:19:57 +0100

    Added Marker to Maps

---
+++ Frontend/src/components/GoogleMaps/Marker.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Marker.module.css';
+
+const Marker = () => {
+  return (
+    <div className={classes.marker}>
+        Marker
+    </div>
+  )
+}
+
+export default Marker;
