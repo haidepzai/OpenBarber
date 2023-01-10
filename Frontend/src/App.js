a7920db14fa71aeafe4a218774042bf65e5ceb27
Hai Vu <zerato@hotmail.de>
2023-01-10 12:59:00 +0100

    Added Validation to Login Form

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,11 +5,15 @@ import ErrorPage from './pages/ErrorPage/ErrorPage';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { useState } from 'react';
+import LoginModal from './components/LoginModal/LoginModal';
 
 function App() {
+  const [loginVisible, setLoginVisible] = useState(false);
+
   return (
     <>
-      <Header />
+      <Header onLogin={() => setLoginVisible(true)} />
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<ErrorPage />} />
@@ -18,6 +22,7 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Footer />
+      {loginVisible && <LoginModal onClose={() => setLoginVisible(false)} />}
     </>
   );
 }
