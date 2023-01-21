45b925d926ca855e41b1312b1c23b1e6fa3ebfe9
Hai Vu <zerato@hotmail.de>
2023-01-21 21:40:32 +0100

    OBA-25 implemented promt to get current location

--- Frontend/src/layout/MySwiper.js
+++ Frontend/src/layout/MySwiper.js
@@ -6,7 +6,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "../css/components/Swiper.css";
 // import required modules
-import {Navigation, Pagination} from "swiper";
+import { Navigation, Pagination } from "swiper";
 
 import beardImage from "../assets/beard.jpg"
 import ratingsImage from "../assets/rating.jpg"
@@ -14,18 +14,19 @@ import mostImage from "../assets/most.jpg"
 import priceImage from "../assets/price.jpg"
 import locationImage from "../assets/location.jpg"
 import tipsImage from "../assets/tips.jpg"
-import {Typography, Box} from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
 
 function MySwiper(props) {
 
     const criteria = [
-        {name: "Best Ratings", src: ratingsImage},
-        {name:"Cheapest Prices", src: priceImage},
-        {name: "Near you", src: locationImage},
-        {name: "With Beard Trimming", src: beardImage},
-        {name: "Most Ratings", src: mostImage},
-        {name: "Our personal tips", src: tipsImage}]
+        { id: 1, name: "Best Ratings", src: ratingsImage },
+        { id: 2, name: "Cheapest Prices", src: priceImage },
+        { id: 3, name: "Near you", src: locationImage },
+        { id: 4, name: "With Beard Trimming", src: beardImage },
+        { id: 5, name: "Most Ratings", src: mostImage },
+        { id: 6, name: "Our personal tips", src: tipsImage }
+    ]
 
     return (
         <div style={{ position: "relative" }}>
@@ -39,33 +40,33 @@ function MySwiper(props) {
                 modules={[Pagination, Navigation]}
                 className="mySwiper"
             >
-                {criteria.map(({ name, src}) => (
-                    <SwiperSlide>
+                {criteria.map((currElement, index) => (
+                    <SwiperSlide key={index}>
                         <Box sx={{
-                                padding: "0",
-                                position: "relative",
-                                height: "300px",
-                                width: "300px",
-                                borderRadius: "10px",
-                                boxShadow: "rgba(0, 0, 0, 0.6) 0px 2px 8px",
-                                transition: "0.15s ease-in-out",
+                            padding: "0",
+                            position: "relative",
+                            height: "300px",
+                            width: "300px",
+                            borderRadius: "10px",
+                            boxShadow: "rgba(0, 0, 0, 0.6) 0px 2px 8px",
+                            transition: "0.15s ease-in-out",
+                            "& img": {
+                                objectFit: "cover", filter: "brightness(0.75) contrast(1.2)", borderRadius: "10px", transition: "0.15s ease-in-out",
+                            },
+                            "&:hover": {
+                                cursor: "pointer",
                                 "& img": {
-                                    objectFit: "cover", filter: "brightness(0.75) contrast(1.2)", borderRadius: "10px", transition: "0.15s ease-in-out",
+                                    filter: "brightness(0.55) contrast(1.2)",
                                 },
-                                "&:hover": {
-                                    cursor: "pointer",
-                                    "& img": {
-                                        filter: "brightness(0.55) contrast(1.2)",
-                                    },
-                                    "& h4": {
-                                        textShadow: "3px 3px 0px #6D5344"
-                                    }
-                                },
-                            }}
+                                "& h4": {
+                                    textShadow: "3px 3px 0px #6D5344"
+                                }
+                            },
+                        }}
                         >
-                            <img src={src} />
-                            <Typography variant="h4" sx={{ textAlign: "center", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", margin: "0", color: "white.main", transition: "0.15s ease-in-out"}}>
-                                {name}
+                            <img src={currElement.src} alt="category"/>
+                            <Typography variant="h4" sx={{ textAlign: "center", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", margin: "0", color: "white.main", transition: "0.15s ease-in-out" }}>
+                                {currElement.name}
                             </Typography>
                         </Box>
                     </SwiperSlide>
