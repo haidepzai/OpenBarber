65cedd8e2eef98686afca70ea6c42de938e0afb3
Hai Vu <zerato@hotmail.de>
2023-01-23 16:41:03 +0100

    Updated Background image for detail view

--- Frontend/src/pages/DetailPage/DetailPage.js
+++ Frontend/src/pages/DetailPage/DetailPage.js
@@ -18,12 +18,12 @@ const BARBER_SHOP = {
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   gallery: [
-    'https://media.istockphoto.com/photos/retro-styled-barbershop-picture-id638568556',
-    'https://media.istockphoto.com/photos/retro-styled-barbershop-picture-id638568556',
-    'https://media.istockphoto.com/photos/retro-styled-barbershop-picture-id638568556',
-    'https://media.istockphoto.com/photos/retro-styled-barbershop-picture-id638568556',
-    'https://media.istockphoto.com/photos/retro-styled-barbershop-picture-id638568556',
-    'https://media.istockphoto.com/photos/retro-styled-barbershop-picture-id638568556',
+    'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    'https://images.unsplash.com/photo-1560066984-138dadb4c035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
   ],
   services: [
     {
