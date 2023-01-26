ac6d5398d1a79444efeb8173ccc9f359a4397d16
Vidovic Kristian <kv021@hdm-stuttgart.de>
2023-01-26 10:51:41 +0100

    connected front-/backend login, added WebConfig, optimized User and AuthRequest Model since we have email instead of username

---
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.hdmstuttgart.mi.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/* ENABLE CORS FOR THE WHOLE APPLICATION */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                /*.allowCredentials(true)*/
+                .maxAge(3600L);
+    }
+}
