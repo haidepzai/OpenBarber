f0f840954d126bffe915a918c66ffa8f7c4d8196
ds189 <Enton!Flegm0n>
2023-02-04 18:49:34 +0100

    add file endpoint

---
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/model/dto/EnterpriseRequest.java
@@ -0,0 +1,23 @@
+package com.hdmstuttgart.mi.backend.model.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.net.URL;
+import java.sql.Blob;
+import java.util.List;
+
+@Data
+public class EnterpriseRequest {
+    private String name;
+    private String address;
+    private String eMail;
+
+    /*private MultipartFile file;*/
+}
---
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/model/dto/EnterpriseResponse.java
@@ -0,0 +1,8 @@
+package com.hdmstuttgart.mi.backend.model.dto;
+
+import lombok.Data;
+
+@Data
+public class EnterpriseResponse {
+    private String name;
+}
