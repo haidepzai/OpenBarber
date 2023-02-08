90e5abd8b8e71f61af1414bc0d9f7236ccf8bfdb
David Redinger <d.redinger@outlook.de>
2023-02-08 16:36:04 +0100

    Added gap data injection for verification.mjml - TODO: confirmationCode logic backend

--- Backend/src/main/java/com/hdmstuttgart/mi/backend/BackendApplication.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/BackendApplication.java
@@ -3,19 +3,11 @@ package com.hdmstuttgart.mi.backend;
 import com.hdmstuttgart.mi.backend.service.EmailSenderService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import org.slf4j.Logger;
 
-import javax.mail.MessagingException;
 
 //@RestController
 //@EnableAutoConfiguration
@@ -29,19 +21,9 @@ public class BackendApplication {
     private EmailSenderService emailSenderService;
 
 
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        System.out.println("tested");
     }
-
-
-/*
-    //Uncomment to send a mail after boot up - configure in service class
-    @EventListener(ApplicationReadyEvent.class)
-    public void sendMail() throws MessagingException {
-        emailSenderService.sendVerificationEmail();
-    }
-*/
-
 
 }
