53bed2c31097ffa7eb8a8bab2401ab979ba0017d
Vidovic Kristian <kv021@hdm-stuttgart.de>
2023-02-27 05:46:25 +0100

    added verfification endpoint and functionality

--- Backend/src/main/java/com/hdmstuttgart/mi/backend/model/enums/UserRole.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/model/enums/UserRole.java
@@ -2,8 +2,8 @@ package com.hdmstuttgart.mi.backend.model.enums;
 
 public enum UserRole {
 
-    UNCONFIRMED,    // unconfirmed e-mail
-    CONFIRMED,      // confirmed e-mail
-    OPERATOR,       // confirmed business owner
+    UNVERIFIED,    // unconfirmed e-mail
+    VERIFIED,      // confirmed e-mail
+    OPERATOR,      // confirmed business owner
 //    ADMIN
 }
