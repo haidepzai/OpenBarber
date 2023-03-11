a3e9a2ecc59f7a5167cb27424ac65f169ab1652b
Hai Vu <zerato@hotmail.de>
2023-03-11 16:44:47 +0100

    Refactored EmailSenderService

--- Backend/src/main/java/com/hdmstuttgart/mi/backend/BackendApplication.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/BackendApplication.java
@@ -4,6 +4,7 @@ import com.hdmstuttgart.mi.backend.service.EmailSenderService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.slf4j.Logger;
@@ -11,16 +12,11 @@ import org.slf4j.Logger;
 
 
 //@RestController
-//@EnableAutoConfiguration
 //@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
     private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
-
-    @Autowired
-    private EmailSenderService emailSenderService;
-
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
--- Backend/src/main/java/com/hdmstuttgart/mi/backend/config/ApplicationConfig.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/config/ApplicationConfig.java
@@ -1,10 +1,14 @@
 package com.hdmstuttgart.mi.backend.config;
 
 import com.hdmstuttgart.mi.backend.repository.UserRepository;
+import io.camassia.mjml.MJMLClient;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -14,11 +18,42 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Properties;
+
 @Configuration
 @RequiredArgsConstructor
+@ComponentScan(basePackages = "com.hdmstuttgart.mi.backend")
 public class ApplicationConfig {
 
     private final UserRepository userRepository;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("openbarber.hdm@gmail.com");
+        mailSender.setPassword("omzprdygnvddylbk");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    @Bean
+    public MJMLClient mjmlClient() {
+        String appId = "08e27ced-848a-4489-891c-e4ac7fa0c612";
+        String mailUsername = "openbarber.hdm@gmail.com";
+        return MJMLClient.newDefaultClient()
+                .withApplicationID(appId)
+                .withApplicationKey(mailUsername);
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
