78b2ec09b2ccbd11f60b4bc4405f14bce8c56c21
Hai Vu <zerato@hotmail.de>
2024-03-24 21:08:33 +0100

    Added new endpoint to retrieve enterprise by radius

--- Backend/src/main/java/com/hdmstuttgart/mi/backend/repository/EnterpriseRepository.java
+++ Backend/src/main/java/com/hdmstuttgart/mi/backend/repository/EnterpriseRepository.java
@@ -3,6 +3,8 @@ package com.hdmstuttgart.mi.backend.repository;
 import com.hdmstuttgart.mi.backend.model.Enterprise;
 import com.hdmstuttgart.mi.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,4 +25,8 @@ public interface EnterpriseRepository extends JpaRepository<Enterprise, Long> {
      * @return the optional
      */
     Optional<Enterprise> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM enterprise WHERE (6371 * acos(cos(radians(:address_latitude)) * cos(radians(address_latitude)) * cos(radians(address_longitude) - radians(:address_longitude)) + sin(radians(:address_latitude)) * sin(radians(address_latitude)))) < :radius", nativeQuery = true)
+    List<Enterprise> findWithinRadius(@Param("address_latitude") double latitude, @Param("address_longitude") double longitude, @Param("radius") double radius);
+
 }
